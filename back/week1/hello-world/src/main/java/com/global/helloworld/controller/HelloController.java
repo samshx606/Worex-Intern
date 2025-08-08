f25b4241278b245c97308f599ac2796e2904a1a5
samshx606 <saaameh.0.1@gamil.com>
2025-08-08 17:25:12 +0300

    Added first backend task

---
+++ back/week1/hello-world/src/main/java/com/global/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.global.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello/api")
+
+public class HelloController {
+    @GetMapping("")
+    public String printHello(){
+        return "Hello world!";
+    }
+}
